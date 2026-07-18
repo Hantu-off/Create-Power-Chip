381e7cf0d0a4aeaba046a59a5e395ff09b86fdfc
AmeliaCute <48628282+AmeliaCute@users.noreply.github.com>
2026-07-18 04:35:25 +0200

    Making chip casing givable from the C:PG Creative Tab

--- src/main/java/xyz/amycute/powerchip/PowerChips.java
+++ src/main/java/xyz/amycute/powerchip/PowerChips.java
@@ -1,7 +1,14 @@
 package xyz.amycute.powerchip;
 
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import org.patryk3211.powergrid.PowerGrid;
+import org.patryk3211.powergrid.forge.PowerGridImpl;
 import xyz.amycute.powerchip.registry.ModItems;
 
 @Mod(PowerChips.MOD_ID)
@@ -12,5 +19,14 @@ public class PowerChips
     public PowerChips(IEventBus modBus)
     {
         ModItems.ITEMS.register(modBus);
+        modBus.addListener(this::addTab);
+    }
+
+    private void addTab(BuildCreativeModeTabContentsEvent event)
+    {
+        if (event.getTabKey().location().equals(ResourceLocation.fromNamespaceAndPath("powergrid", "main")))
+        {
+            event.accept(ModItems.CHIP_CASING.get());
+        }
     }
 }
